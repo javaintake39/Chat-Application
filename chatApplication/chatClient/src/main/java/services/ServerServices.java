91334e227607da5909a8234bf4dbe22fdfbbce4a
Arafa <javaintake@gmail.com>
2019-02-15 18:12:05 +0200

    ServerService
    Try to implement

--- chatApplication/chatClient/src/main/java/services/ServerServices.java
+++ chatApplication/chatClient/src/main/java/services/ServerServices.java
@@ -5,31 +5,40 @@
  */
 package services;
 
+import eg.gov.iti.chatcommon.model.User;
 import eg.gov.iti.chatcommon.rmiconnection.ServerInterface;
-import java.rmi.AccessException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author pc
  */
 public class ServerServices {
-    private ServerInterface serverRefrence;
-     public ServerServices() {
-         try {
 
-            Registry registry = LocateRegistry.getRegistry("127.0.0.1", 6000);
-            serverRefrence = (ServerInterface) registry.lookup("chat");
+    private ServerInterface serverRefrence;
+
+    public ServerServices() {
+        try {
+
+            Registry registry = LocateRegistry.getRegistry("127.0.0.1", 9800);
+            serverRefrence = (ServerInterface) registry.lookup("chatService");
         } catch (RemoteException ex) {
             ex.printStackTrace();
         } catch (NotBoundException ex) {
             ex.printStackTrace();
         }
-     }
+    }
     
+    public void register(User clientService) {
+        try {
+            serverRefrence.loginIn(clientService);
+        } catch (RemoteException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
 }
