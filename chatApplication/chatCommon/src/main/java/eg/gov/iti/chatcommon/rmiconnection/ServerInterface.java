1c9f03a42ae34c9e9edd0efac362fd0686c99bc7
YoyaMohamed <aya.yoya962015@gmail.com>
2019-02-24 15:23:48 +0200

    for timeStamp

--- chatApplication/chatCommon/src/main/java/eg/gov/iti/chatcommon/rmiconnection/ServerInterface.java
+++ chatApplication/chatCommon/src/main/java/eg/gov/iti/chatcommon/rmiconnection/ServerInterface.java
@@ -29,6 +29,7 @@ public interface ServerInterface extends Remote{
 
     
     //Add fiiend Functions
+    
     public  List<String> getAllContactsNumber() throws RemoteException;
     public void SendInvitation(List<String> contacts,String senderPhone)throws RemoteException;
     public List<User> viewInvitation(String reciverPhone) throws RemoteException; 
