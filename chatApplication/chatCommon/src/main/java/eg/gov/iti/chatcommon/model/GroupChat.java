e7ade301360cda3f35eace4fb30164a4592d8e34
YoyaMohamed <aya.yoya962015@gmail.com>
2019-02-26 09:55:17 +0200

    Commit

---
+++ chatApplication/chatCommon/src/main/java/eg/gov/iti/chatcommon/model/GroupChat.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eg.gov.iti.chatcommon.model;
+
+
+public class GroupChat {
+    private int ID;
+    private String Name;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public byte[] getPicture() {
+        return picture;
+    }
+
+    public void setPicture(byte[] picture) {
+        this.picture = picture;
+    }
+    private byte [] picture;
+    
+    
+}
